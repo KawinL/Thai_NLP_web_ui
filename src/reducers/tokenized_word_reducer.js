ccedb6f41d7f13a5dd9b4f31a56c66ee71b61014
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2018-02-04 22:07:54 +0700

    Add defult for error to communicate with API

--- src/reducers/tokenized_word_reducer.js
+++ src/reducers/tokenized_word_reducer.js
@@ -5,7 +5,10 @@ export default function(state = [], action){
     console.log(action.payload)
     switch(action.type){
     case TOKENIZE_WORD:
+        if (!action.payload.data) return ["something","wrong"]; 
         return action.payload.data.string_list;
+    default:
+        return []
     }
     return state;
 }
