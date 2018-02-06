ae971b27bc1b61011cc040046457855d8516825c
Jijy Thamjarat <jijy.chavisa@gmail.com>
2018-02-06 16:26:06 +0700

    edit nav bar color

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class NavBar extends Component {
   render() {
-      return <nav class="navbar navbar-expand-lg navbar-light bg-success rounded">
+      return <nav class="navbar-expand-lg navbar navbar-inverse bg-inverse">
           <a class="navbar-brand float-left">Thai NLP Platform</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon" />
