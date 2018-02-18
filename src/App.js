384a68cdc3e45b110feefc2fc657a55a0e1d145f
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2018-02-19 00:51:31 +0700

    change tab name

--- src/App.js
+++ src/App.js
@@ -23,38 +23,32 @@ class App extends Component {
             <div class="row">
               <div class="container-fluid col-12 mt-4 mb-4">
                 <div class="mt-4 mb-4">
-                  <ExplainUI topic="API DEMO" explanation={<p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing
-                        elit. Animi, alias. Dolor error sint nihil nemo
-                        commodi maxime excepturi tenetur quos atque,
-                        voluptates tempora corrupti recusandae minima
-                        asperiores? In, assumenda rerum.
-                      </p>} />
+                  <ExplainUI topic="API DEMO" explanation={<p />} />
                 </div>
                 <hr />
               </div>
               <div class="col-lg-3 col-md-3 col-sm-12">
                 <div class="container-fluid ">
-                  <MenuUI head="Foundation" detail={{ Tokenizer: "/tokenizer", "Word Embedder": "/word-embedder", "Name Entity Recognizer": "/ner", "Part of Speech Tagger": "/pos" }} />
-                  <MenuUI head="Application" detail={{ "Sentiment Analysis": "/sentiment-analyser", "Text classifier": "/text-classifier", "Keyword Expansion": "/keyword-expansion" }} />
+                  <MenuUI head="Foundation" detail={{ "Word Tokenization": "/tokenization", "Word Embedding": "/word-embedding", "Name Entity Recognition": "/ner", "Part of Speech Tagging": "/pos" }} />
+                  <MenuUI head="Application" detail={{ "Sentiment Analysis": "/sentiment-analyser", "Text Categorization": "/text-categorization", "Keyword Expansion": "/keyword-expansion" }} />
                 </div>
               </div>
               <div class="container-fluid col-lg-9 col-md-9 col-sm-12">
                 <Redirect exact from="/" to="/ner" />
-                <Route exact path="/tokenizer" component={TokenizerUI} />
-                <Route exact path="/word-embedder" component={WordEmbedderUI} />
+                <Route exact path="/tokenization" component={TokenizerUI} />
+                <Route exact path="/word-embedding" component={WordEmbedderUI} />
 
                 <Route exact path="/ner" component={NERUI} />
                 <Route exact path="/pos" component={PosUI} />
 
                 <Route exact path="/keyword-expansion" component={KeywordExpansionUI} />
-                <Route exact path="/text-classifier" component={TextClassifyUI} />
+                <Route exact path="/text-categorization" component={TextClassifyUI} />
                 <Route exact path="/sentiment-analyser" component={SentimentUI} />
               </div>
             </div>
           </div>
           <footer class="footer mt-4 pt-4">
-            <div class="card text-center">
+            <div class="card">
               <div class="card-body bg-dark" />
             </div>
           </footer>
