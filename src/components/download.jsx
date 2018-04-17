8a2bc774d4153324d7e2cc707c41e20a0c47e358
jijyisme <jijy.chavisa@gmail.com>
2018-04-17 16:05:46 +0700

    add class name

---
+++ src/components/download.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import { vectorizeWord } from "../action/index";
+import ResultUI from "./result";
+import ExplainUI from "./explanation";
+import InputUI, { typeOfInputValue } from "./input";
+import ExampleUI from "./example";
+import DropdownUI from "./dropdown";
+class SentimentUI extends Component {
+
+  render() {
+    return (<div class="container">
+        <h1> Get Bailarn Library </h1>
+    </div>)
+  }
+}
+
+export default (SentimentUI);
--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -1,26 +1,52 @@
 import React, { Component } from "react";
-import "./style.css";
+import "./nav_bar.css";
+import classNames from 'classnames';
 
 
 class NavBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {active_api: false,
+                  active_about: false};
+    this.setState= this.setState.bind(this);
+  }
+
+  setAPIActive(){
+    this.setState({ active_api: true,
+                    active_about: false})
+  }
+  setAboutActive(){
+    this.setState({ active_api: false,
+                    active_about: true})
+  }
   render() {
+        const classes = classNames({
+        "nav-item":true,
+        active: this.state.active_api, // only add this class if the state says so
+        });
+        const classes2 = classNames({
+        "nav-item":true,
+        active: this.state.active_about, // only add this class if the state says so
+        });
       return <nav class="navbar navbar-expand-lg navbar-dark text-white justify-content-between" style={{'background':'#545454'}}>
           
           <a class="navbar-brand">
-          <i class="fa fa-leaf w3-large d-inline-block align-top" style={{'color':'#2BD1A3', 'padding-right':'20px'}}></i> 
+          <i class="fa fa-leaf w3-large d-inline-block align-top" style={{'color':'#2BD1A3'}}></i> 
           Bailarn Library</a>
 
           <div class="collapse navbar-collapse float-right text-white " id="navbarsExample09">
             <ul class="navbar-nav mr-auto ">
               
               <li class="nav-item">
-                <a class="nav-link">
+                <a class="nav-link" href="https://github.com/KawinL/Thai_NLP_platform/">
                   <i class="fa fa-cloud-download w3-large"></i> 
                   <span class="pad-left">Download</span>
                 </a>
               </li>
               
-              <li class="nav-item">
+              <li className={classes} onClick={() => this.setAPIActive()}>
                 <a class="nav-link">
                   <i class="fa fa-book w3-large"></i> 
                   <span class="pad-left">API</span>
@@ -29,7 +55,7 @@ class NavBar extends Component {
               </li>
               
 
-              <li class="nav-item">
+              <li className={classes2} onClick={() => this.setAboutActive()}>
                 <a class="nav-link">
                   <i class="fa fa-comments w3-large"></i> 
 
