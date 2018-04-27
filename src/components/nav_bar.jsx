e9808ab4adcca654b02aa07561afbeff83c199cd
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2018-04-27 11:34:11 +0700

    It should be the last one before demo

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -31,64 +31,62 @@ class NavBar extends Component {
         "nav-item":true,
         active: this.state.active_about, // only add this class if the state says so
         });
-      return (
-        <nav class="navbar navbar-default navbar-expand-lg navbar-dark text-white" style={{'background':'#545454'}}>
-          <div class="row" style={{'padding-left': '5px'}}>
-            <img src={logo} height="32" width="32"/>
-              <div class="col-xs-6">
-                <a class="navbar-brand" href="/home">
-                  Bailarn Library
-                </a>
-                <a href="#menu-toggle" class="btn" style={{'background':'#545454','color':'#fff'}} id="menu-toggle">
-                  <i class="fa fa-bars w3-large"></i> 
-                </a>
-              </div>
-              <div class="col-xs-3" style={{"padding":"0"}}>
-                  <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    More
-                  </a>
+      return <nav class="navbar navbar-default navbar-expand-lg navbar-dark text-white" style={{ background: "#545454" }}>
+          <div class="row" style={{ "padding-left": "5px" }}>
+            <img src={logo} height="32" width="32" />
+            <div class="col-xs-6">
+              <a class="navbar-brand" href="/home">
+                Bailarn Library
+              </a>
+              <a href="#menu-toggle" class="btn" style={{ background: "#545454", color: "#fff" }} id="menu-toggle">
+                <i class="fa fa-bars w3-large" />
+              </a>
+            </div>
+            <div class="col-xs-3" style={{ padding: "0" }}>
+              <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                More
+              </a>
               <div class="dropdown-menu right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="https://github.com/KawinL/Thai_NLP_platform/">Download</a>
-                <a class="dropdown-item" href="https://github.com/KawinL/Thai_NLP_platform/tree/master/docs">Documentation</a>
-                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="https://github.com/KawinL/Thai_NLP_platform_for_Demoday/">
+                  Download
+                </a>
+                <a class="dropdown-item" href="https://github.com/KawinL/Thai_NLP_platform_for_Demoday/tree/master/docs">
+                  Documentation
+                </a>
+                <div class="dropdown-divider" />
                 <Link to={"/about"} activeClassName={"active"} className={"dropdown-item"}>
-
                   <span class="pad-left">About Us</span>
                 </Link>
               </div>
-              </div>
-
+            </div>
           </div>
-          <div class="text-white right" id = "nav-item-list" id="navbarButton">
+          <div class="text-white right" id="nav-item-list" id="navbarButton">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item">
                 <a class="nav-link" href="https://github.com/KawinL/Thai_NLP_platform/">
-                  <i class="fa fa-cloud-download w3-large"></i>
+                  <i class="fa fa-cloud-download w3-large" />
                   <span class="pad-left">Download</span>
                 </a>
               </li>
 
               <li className={classes} onClick={() => this.setAPIActive()} id="navbarButton">
                 <a class="nav-link" href="https://github.com/KawinL/Thai_NLP_platform/tree/master/docs">
-                  <i class="fa fa-book w3-large"></i>
+                  <i class="fa fa-book w3-large" />
                   <span class="pad-left">Documentation</span>
                   <span class="sr-only ">(current)</span>
                 </a>
               </li>
 
-
               <li className={classes2} onClick={() => this.setAboutActive()} id="navbarButton">
                 <Link to={"/about"} activeClassName={"active"} className={"nav-link"}>
-
-                  <i class="fa fa-comments w3-large"></i>
+                  <i class="fa fa-comments w3-large" />
 
                   <span class="pad-left">About Us</span>
                 </Link>
               </li>
             </ul>
           </div>
-        </nav>
-      );
+        </nav>;
   }
 }
 
