696c16b2f9725154334f7686009eca6247708969
kawinl <liaokangtai@gmail.com>
2018-05-14 21:37:12 +0700

    Delete files

--- src/script/color_generator.js
+++
@@ -1,6 +0,0 @@
-export default class ColorGenerator{
-    constructor(){
-        this.index = 0
-        this.color = ["AntiqueWhite", "BlueViolet", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "CornflowerBlue", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
-    }
-}
